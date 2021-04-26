807a6484d4f56c46140f8053ad694afaf35851da
EDave64 <nord3rn@gmail.com>
2021-04-26 22:54:33 +0200

    Stuff

---
+++ android/app/src/main/kotlin/com/example/flutter_mahjong_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_mahjong_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ scripts/test.dart
@@ -0,0 +1,333 @@
+import 'package:dart_benchmark/dart_benchmark.dart';
+import 'package:flutter_app/engine/layouts/layout.dart';
+import 'package:flutter_app/engine/layouts/top_down_generator.dart';
+
+Future<void> main() async {
+  Layout layout = layoutDefault();
+  final layoutPrecalc = layout.getPrecalc();
+
+  List<int> retriesCollector = [];
+
+  const tries = 5000;
+
+  await DartBenchmark('Slow Benchmark', () {
+    var retries = 0;
+    while (true) {
+      try {
+        final board = makeBoard(layout, layoutPrecalc);
+        break;
+      } catch (e) {
+        retries++;
+      }
+    }
+    retriesCollector.add(retries);
+  }, count: tries, warmup: false)
+      .run();
+
+  final lowest = retriesCollector.fold<int>(
+      9999999,
+      (previousValue, element) =>
+          element < previousValue ? element : previousValue);
+
+  final highest = retriesCollector.fold<int>(
+      0,
+      (previousValue, element) =>
+          element > previousValue ? element : previousValue);
+
+  final sum = retriesCollector.fold<int>(
+      0, (previousValue, element) => element + previousValue);
+
+  print(
+      "min retries: $lowest, max retries: $highest, avg retries: ${sum / tries}");
+}
+
+List<List<List<bool>>> parseLayout(List<List<String>> str) {
+  return str
+      .map((layer) => layer
+          .map((row) =>
+              row.split('').map((t) => t == 'x').toList(growable: false))
+          .toList(growable: false))
+      .toList(growable: false);
+}
+
+Layout layoutDefault() {
+  return Layout(parseLayout([
+    [
+      "   x x x x x x x x x x x x      ",
+      "                                ",
+      "       x x x x x x x x          ",
+      "                                ",
+      "     x x x x x x x x x x        ",
+      "                                ",
+      "   x x x x x x x x x x x x      ",
+      " x                         x x  ",
+      "   x x x x x x x x x x x x      ",
+      "                                ",
+      "     x x x x x x x x x x        ",
+      "                                ",
+      "       x x x x x x x x          ",
+      "                                ",
+      "   x x x x x x x x x x x x      ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "         x x x x x x            ",
+      "                                ",
+      "         x x x x x x            ",
+      "                                ",
+      "         x x x x x x            ",
+      "                                ",
+      "         x x x x x x            ",
+      "                                ",
+      "         x x x x x x            ",
+      "                                ",
+      "         x x x x x x            ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "           x x x x              ",
+      "                                ",
+      "           x x x x              ",
+      "                                ",
+      "           x x x x              ",
+      "                                ",
+      "           x x x x              ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "             x x                ",
+      "                                ",
+      "             x x                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "              x                 ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ]
+  ]));
+}
+
+Layout layoutDifficult() {
+  return Layout(parseLayout([
+    [
+      "x                              x",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "x                              x",
+    ],
+    [
+      "                                ",
+      " x                            x ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      " x                            x ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "  x                          x  ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "  x                          x  ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "   x                        x   ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "   x                        x   ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "    x                      x    ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "    x                      x    ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "     x                    x     ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "     x                    x     ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "      x                  x      ",
+      "                                ",
+      "                                ",
+      "      x                  x      ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "       x                x       ",
+      "       x                x       ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ],
+    [
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "        xxxxxxxxxxxxxxxx        ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+      "                                ",
+    ]
+  ]));
+}
+
+Layout layoutUnsolvable() {
+  return Layout(parseLayout([
+    [
+      "x",
+    ],
+    [
+      "x",
+    ],
+    [
+      "x",
+    ],
+    [
+      "x",
+    ],
+    [
+      "x",
+    ]
+  ]));
+}
